534f4ef69cc2de888a7e427420a1a97cc3486925
Karim Kammoun <128716985+KarimKammoun@users.noreply.github.com>
2023-03-27 23:55:53 +0100

    Delete Niveau 1/1 – Affichage de texte, suite d'instructions directory

--- Niveau 1/1 – Affichage de texte, suite d'instructions/4) Dans le fourré  .py
+++
@@ -1,8 +0,0 @@
-from robot import *
-for i in range (3):
-    haut()
-droite()
-droite()
-bas()
-bas()
-droite()
--- Niveau 1/1 – Affichage de texte, suite d'instructions/6) Recette secrète  .py
+++
@@ -1,7 +0,0 @@
-from robot import *
-remplir(5)
-transferer(5, 3)
-vider(3)
-transferer(5, 3)
-remplir(5)
-transferer(5, 3)
