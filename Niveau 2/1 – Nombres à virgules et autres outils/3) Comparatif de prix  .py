2a7352a05b9d831686731e17678608b94bca39e2
Karim Kammoun <128716985+KarimKammoun@users.noreply.github.com>
2023-03-30 23:09:37 +0100

    France-IOI

---
+++ Niveau 2/1 – Nombres à virgules et autres outils/3) Comparatif de prix  .py
@@ -0,0 +1,6 @@
+a=int(input())
+for i in range(a):
+    b=float(input())
+    c=float(input())
+    d=float(input())
+    print(d/b)
---
+++ Niveau 2/1 – Nombres à virgules et autres outils/4) Moyenne des notes  .py
@@ -0,0 +1,5 @@
+a=int(input())
+b=0
+for i in range(a):
+    b=b+float(input())
+print(b/a)
